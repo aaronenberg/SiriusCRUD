a0d884fe89691ce06b8fdf7afaaf3fe93e465dab
aaron <aaron.enberg@gmail.com>
2019-02-17 11:35:30 -0800

    rm unused imports

--- sirius/articles/views.py
+++ sirius/articles/views.py
@@ -1,8 +1,4 @@
-from rest_framework import status, viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.renderers import BrowsableAPIRenderer
+from rest_framework import viewsets
 from .serializers import ArticleSerializer
 from .models import Article
 
