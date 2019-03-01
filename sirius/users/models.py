4947a7669b4b8a783084c111e21c46b59e26a383
aaron <aaron.enberg@gmail.com>
2019-03-01 08:32:10 -0800

    copy Django AbstractUser stuff to BaseUser

--- sirius/users/models.py
+++ sirius/users/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 STUDENT = 'ST'
@@ -14,34 +17,44 @@ ACCOUNT_TYPES = (
 
 class BaseUserManager(BaseUserManager):
 
-    def _create_user(self, email, password, account_type, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError('Email is required.')
-        if not account_type:
-            raise ValueError('Account type is required.')
-        user = self.model(email=self.normalize_email(email), account_type=account_type)
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email,  password, account_type, **extra_fields):
-        user = self._create_user(email, account_type, password)
-        user.save(using=self._db)
-        return user
+    def create_user(self, email,  password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('account_type', STUDENT)
+        return self._create_user(email, password)
 
-    def create_superuser(self, email, password, account_type, **extra_fields):
-        user = self._create_user(email, password, account_type)
-        user.is_staff = True
-        user.save(using=self._db)
-        return user
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('account_type', FACULTY)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('account_type') is not FACULTY:
+            raise ValueError('Superuser must have account type="FA"')
+        return self._create_user(email, password, **extra_fields)
 
 
-class BaseUser(AbstractBaseUser):
+class BaseUser(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(
         max_length=255,
         unique=True,
         verbose_name='School Email',
+        error_messages={
+            'unique': _("A user with that email already exists."),
+        },
     )
     account_type = models.CharField(
         max_length=2,
@@ -50,12 +63,12 @@ class BaseUser(AbstractBaseUser):
         verbose_name='Account Type'
     )
     first_name = models.CharField(
-        max_length=255,
+        max_length=30,
         blank=True,
         verbose_name='First Name'
     )
     last_name = models.CharField(
-        max_length=255,
+        max_length=150,
         blank=True,
         verbose_name='Last Name'
     )
@@ -64,27 +77,28 @@ class BaseUser(AbstractBaseUser):
         blank=True,
         verbose_name='MI'
     )
-    date_joined = models.DateField(
-        auto_now_add=True,
-        verbose_name='Date Joined'
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
     )
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
     objects = BaseUserManager()
 
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = USERNAME_FIELD
-    REQUIRED_FIELDS = ['account_type', ]
 
     def __str__(self):
         return self.email
-
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
 
     @property
     def is_privileged(self):
@@ -92,4 +106,8 @@ class BaseUser(AbstractBaseUser):
 
     class Meta:
         verbose_name_plural = "Users"
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
 
