8554b3ce0ad8083487dfa02742201a5cb2961e1d
Vang Thao <vangthao@csus.edu>
2019-03-08 01:47:40 -0800

    Fixed merge conflict

--- sirius/articles/views.py
+++ sirius/articles/views.py
@@ -152,8 +152,7 @@ class SearchPageView(TemplateView):
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
-		#context['articles'] = Article.objects.order_by('-created')
-		context['articles'] =  Article.objects.exclude(Q(is_public=False))
+		context['articles'] = Article.objects.exclude(Q(is_public=False)).order_by('-created')
 		context['subjects'] = Article.objects.order_by('subject').distinct('subject')
 		context['courses'] = Course.objects.all()
 		return context
