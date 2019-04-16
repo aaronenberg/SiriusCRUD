2d89f143ce719535620e68deeffc7c96dab1ba6d
aaron <aaron.enberg@gmail.com>
2019-04-16 00:58:17 -0700

    fix syntax

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,7 +10,7 @@ if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME']
+            'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
