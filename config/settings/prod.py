c906ec5aef8537bb46a64337b936d48da36b4877
aaron <aaron.enberg@gmail.com>
2019-04-22 21:51:28 -0700

    add ses

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -46,7 +46,7 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django_ses.SESBackend'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = get_env_var('SERVER_EMAIL')
 SERVER_EMAIL = get_env_var('SERVER_EMAIL')
@@ -68,6 +68,9 @@ AWS_S3_OBJECT_PARAMETERS = {
     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
     'CacheControl': 'max-age=94608000',
 }
+
+AWS_SES_REGION_NAME = 'us-west-2'
+AWS_SES_REGION_ENDPOINT = 'email.us-west-2.amazonaws.com'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_LOCATION = 'static'
